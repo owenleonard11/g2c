9b48df79b3bc320df39f9b4ce1749f2817aa2854
Owen Leonard <owenleonard11@gmail.com>
2024-11-20 21:50:46 -0800

    Refcator Zotero fetch into class

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,40 +8,57 @@ export enum Layer {
     Train
 }
 
-// citekey: {Author|Title}{YYYY}{1+}
-const groupUrl = "https://api.zotero.org/groups/5766383/";
-export const fetchBib = () => {
-    const citekeyToZid: Map<string, string> = new Map()
-    
-    fetch(groupUrl + 'items')
-        .then(response => {
-            return response.json();
-        })
-        .then(items => {
-            for (const item in items) {
-                const meta = items[item]['meta'];
-                const data = items[item]['data'];
+export class ZoteroBib {
+    readonly url: string;
+    citeFormat: string;
+    citeMap: Promise<Map<string, string>> | null;
 
-                const citekeyFirst  = Object.hasOwn(meta, 'creatorSummary') ? meta['creatorSummary'] : data['title'].split(' ').slice(0, 2).join('');
-                const citekeySecond = meta['parsedDate'].slice(0, 4)
-                let citekeyNo = 0
-                let citekey   = citekeyFirst + citekeySecond + (citekeyNo ? citekeyNo : "")
-                while(citekeyToZid.has(citekey)) {
-                    citekey   = citekeyFirst + citekeySecond + (citekeyNo++ ? citekeyNo : "")
+    constructor(url: string, citeFormat: string, buildCiteMap=true) {
+        this.url = url;
+        this.citeFormat = citeFormat;
+        
+        if (buildCiteMap) {
+            this.citeMap = fetch(this.url + 'items').then(
+                response => { return response.json() }
+            ).then(
+                items => {
+                    let citeMap: Map<string, string> = new Map();
+    
+                    for (const item in items) {
+                        const meta = items[item]['meta'];
+                        const data = items[item]['data'];
+        
+                        const citekeyFirst  = Object.hasOwn(meta, 'creatorSummary') ? meta['creatorSummary'] : data['title'].split(' ').slice(0, 2).join('');
+                        const citekeySecond = meta['parsedDate'].slice(0, 4)
+                        let citekeyNo = 0
+                        let citekey   = citekeyFirst + citekeySecond + (citekeyNo ? citekeyNo : "")
+                        while(citeMap.has(citekey)) {
+                            citekey = citekeyFirst + citekeySecond + (citekeyNo++ ? citekeyNo : "")
+                        }
+                        citeMap.set(citekey, data.key)
                 }
-                citekeyToZid.set(citekeyFirst + citekeySecond + (citekeyNo ? citekeyNo : ""), data.key)
-            }
-        });
     
-    return citekeyToZid;
-}
+                return citeMap;
+            })
+        } else {
+            this.citeMap = null;
+        }
+        
+    }
 
-export const fetchCite = (id: string) => {
-    fetch(`https://api.zotero.org/groups/5766383/items/${id}/?format=json&include=bib&style=apa`)
-        .then(response => {
-            return response.json()
-        })
-        .then(bib => {
-            console.log(bib)
-        })
+    getCitationById(id: string): Promise<string> {
+        let requestUrl = `${this.url}/items/${id}/?format=json&include=bib&style=apa`
+        return fetch(requestUrl)
+            .then(response => { return response.json() })
+            .then(item => { return item['bib'] || '' })
+    }
+
+    getCitationByCitekey(citekey: string): Promise<string> {
+        if (!this.citeMap) {
+            throw new Error('No citeMap built')
+        }
+
+        let id: Promise<string> = this.citeMap.then(citeMap => { return citeMap.get(citekey) ||  '' })
+        return id.then(id => { return this.getCitationById(id) })
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,17 +4,25 @@
 	import '@fortawesome/fontawesome-free/css/all.min.css'
 
 	import { currentLayer } from "$lib/../stores";
-	import { fetchBib, Layer } from '$lib/utils';
+	import { ZoteroBib, Layer } from '$lib/utils';
 	import LayerCard from "$lib/components/LayerCard.svelte";
 
 	import favicon from '$lib/images/favicon.svg';
 	import zotero from '$lib/images/zotero.svg';
 	import github from '$lib/images/github.svg';
 
-	const LAYER_NAMES  = ['Home', 'Extract', 'Produce', 'Transport', 'Connect', 'Compute', 'Train']
-	const LAYER_COLORS = ['#111928', '#a56b47', '#c9bf96', '#c49b87', '#9cbeaf', '#79a657', '#fbcb7d']
+	const LAYER_NAMES  = ['Home', 'Extract', 'Produce', 'Transport', 'Connect', 'Compute', 'Train'];
+	const LAYER_COLORS = ['#111928', '#a56b47', '#c9bf96', '#c49b87', '#9cbeaf', '#79a657', '#fbcb7d'];
 
-	const zoteroIds = fetchBib();
+	let bib = new ZoteroBib('https://api.zotero.org/groups/5766383/', 'apa');
+	let citation = '';
+	const show = (citekey: string) => {
+		console.log("Called show")
+		bib.getCitationByCitekey(citekey).then(citekey => {
+			console.log("got bib: " + citekey)
+			citation = citekey;
+		})
+	}
 	
 	let layer = Layer.Home; let showMenu = true; let rightColor = LAYER_COLORS[Layer.Home];
 	currentLayer.subscribe((value) => layer = value);
@@ -66,7 +74,11 @@
 				<p class="xl:text-xl mt-4 text-justify">
 					Ground to Cloud (G2C) is a non-exhaustive repository of scholarly research and investigative journalism on the 
 					numerous infrastructures that support contemporary artificial intelligence. Inspired by Benjamin Bratton's "stack"
-					and Kate Crawford's "atlas," G2C has organized a selection of this work into the layers you see on the left. Select one to
+					and 
+					<button class="inline border-2 border-[#a56b47] px-1 rounded-md" on:click={() => show("Crawford2021")}>
+						Kate Crawford's "atlas"
+					</button>,
+					G2C has organized a selection of this work into the layers you see on the left. Select one to
 					browse sites of interest and accompanying resources. You can return to this page at any time by clicking the 
 					<img src={favicon} alt="Ground to Cloud Favicon" class="inline h-[1.875rem] p-0.5" />
 					icon at the top left.
@@ -109,6 +121,7 @@
 	</div>
 	<div class="h-lvh w-full px-[6dvh] pt-[4dvh] overflow-y-scroll">
 		<div class="flex flex-col text-black">
+			{@html citation}
 			{#if layer == Layer.Home}
 			<div transition:slide>
 
