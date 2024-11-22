98e76297be7f4a85a4d2cdebf3fe4be93082e4d6
Owen Leonard <owenleonard11@gmail.com>
2024-11-21 17:50:19 -0800

    Add positioning logic to Citation

--- src/lib/components/Citation.svelte
+++ src/lib/components/Citation.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import type { ZoteroBib } from "$lib/utils";
     import { onMount } from "svelte";
-    enum xDirection { Auto, Left, Right }
-    enum yDirection { Auto, Up, Down }
+    enum xDirection { Auto, Left, Center, Right }
+    enum yDirection { Auto, Up, Middle, Down }
 
     export let bib:     Promise<ZoteroBib>;
     export let citekey: string    = "";
-    // export let dir:     Direction = Direction.Auto;
+    export let xDir:    xDirection = xDirection.Center;
+    export let yDir:    yDirection = yDirection.Auto;
 
     let showCitation = false;
     let citation = '';
@@ -20,20 +21,58 @@
         })
     )
 
-    let x: number = 0;
-    let y: number = 0;
+    let windowWidth: number;
+    let windowHeight: number;
+
+    let positionStyle: string = ""
+    const calcPosStyle = (x: number, y: number) => {
+        // calculate direction
+        let useXDir: xDirection = xDir;
+        let useYDir: yDirection = yDir;
+        if (useXDir == xDirection.Auto) {
+            useXDir = x >= (windowWidth / 2) ? xDirection.Left : xDirection.Right;
+        }
+        if (useYDir == yDirection.Auto) {
+            useYDir = y >= (windowHeight / 2) ? yDirection.Up : yDirection.Down;
+        }
+
+        // calculate position
+        let xPos:   string = `left:${x}px`;
+        let yPos:   string = `top:${y}px`;
+        let xTrans: string = '';
+        let yTrans: string = '';
+
+        switch (useXDir) {
+            case xDirection.Right:  xTrans = '0';    break;
+            case xDirection.Center: xTrans = '-50';  break;
+            case xDirection.Left:   xTrans = '-100'; break;
+        }
+        switch (useYDir) {
+            case yDirection.Down:   yTrans = '0';    break;
+            case yDirection.Middle: yTrans = '-50';  break;
+            case yDirection.Up:     yTrans = '-100'; break;
+        }
+
+        return `${xPos};${yPos};transform:translate(${xTrans}%, ${yTrans}%)`;
+    }
 </script>
 
-<a  href="https://www.zotero.org/groups/5"
-    class="{showCitation ? "" : "hidden"} bg-[#9cbeaf] absolute top-0 p-2 text-sm max-w-[20dvw] text-left"
-    style="left:{x}px;top:{y}px"
+<svelte:window bind:innerWidth={windowWidth} bind:innerHeight={windowHeight} />
+
+<div
+    class="{showCitation ? "" : "hidden"} bg-[#9cbeaf] absolute top-0 p-2 text-sm w-[24dvw] text-left"
+    style="{positionStyle}"
 >     
-    {@html citation}
-</a>
+    {#if citation}
+        {@html citation}
+    {:else}
+        Loading citation...
+    {/if}
+</div>
 
 <a 
-    class="bg-[#9cbeaf] rounded-md px-1 [&>*]:pointer-events-none" href={citationUrl} target="_blank"
-    on:mouseenter={(e) => { showCitation = true; x = e.clientX; y=e.clientY}}
+    class="bg-[#9cbeaf] rounded-md px-1" href={citationUrl} target="_blank"
+    on:mouseenter={(e) => { showCitation = true; positionStyle = calcPosStyle(e.clientX, e.clientY)}}
     on:mouseleave={() => { showCitation = false }}
 >
     <slot />
