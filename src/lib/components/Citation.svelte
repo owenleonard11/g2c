5c71be325f741b698911362e10811b4584f6a871
Owen Leonard <owenleonard11@gmail.com>
2025-01-12 15:23:27 -0800

    Fix citation popup behavior

--- src/lib/components/Citation.svelte
+++ src/lib/components/Citation.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { writable, type Writable } from "svelte/store";
+
     enum xDirection { Auto, Left, Center, Right }
     enum yDirection { Auto, Up, Middle, Down }
 
@@ -11,12 +13,48 @@
 
     let windowWidth: number;
     let windowHeight: number;
+    let citationWidth: number;
+    let citationHeight: number;
 
-    let positionStyle: string = ""
+    let positionStyle: Writable<string> = writable("")
     const calcPosStyle = (x: number, y: number) => {
         // calculate direction
         let useXDir: xDirection = xDir;
         let useYDir: yDirection = yDir;
+
+        // check distance to left
+        if (useXDir == xDirection.Left && x - citationWidth < 0.4 * windowHeight) {
+            useXDir = xDirection.Center;
+        }
+        if (useXDir == xDirection.Center && x - citationWidth / 2 < 0.4 * windowHeight) {
+            useXDir = xDirection.Right;
+        }
+
+        // check distance to right
+        if (useXDir == xDirection.Right && x + citationWidth > windowWidth) {
+            useXDir = xDirection.Center;
+        }
+        if (useXDir == xDirection.Center && x + citationWidth / 2 > windowWidth) {
+            useXDir = xDirection.Left;
+        }
+
+        // check distance to top
+        if (useYDir == yDirection.Up && y - citationHeight < 0) {
+            useYDir = yDirection.Middle;
+        }
+        if (useYDir == yDirection.Middle && y - citationHeight / 2 < 0) {
+            useYDir = yDirection.Down;
+        }
+
+        // check distance to bottom
+        if (useYDir == yDirection.Down && y + citationHeight > windowHeight) {
+            useYDir = yDirection.Middle;
+        }
+        if (useYDir == yDirection.Middle && y + citationHeight / 2 > windowHeight) {
+            useYDir = yDirection.Up;
+        }
+
+        // set auto directions
         if (useXDir == xDirection.Auto) {
             useXDir = x >= (windowWidth / 2) ? xDirection.Left : xDirection.Right;
         }
@@ -41,22 +79,22 @@
             case yDirection.Up:     yTrans = '-100'; break;
         }
 
-        return `${xPos};${yPos};transform:translate(${xTrans}%, ${yTrans}%)`;
+        positionStyle.set(`${xPos};${yPos};transform:translate(${xTrans}%, ${yTrans}%)`);
     }
 </script>
 
 <svelte:window bind:innerWidth={windowWidth} bind:innerHeight={windowHeight} />
 
-<div
-    class="{showCitation ? "" : "hidden"} bg-[#9cbeaf] absolute top-0 p-2 text-sm w-[24dvw] text-left"
-    style="{positionStyle}"
+<div bind:clientWidth={citationWidth} bind:clientHeight={citationHeight} 
+    style="{$positionStyle}; opacity: {showCitation ? 1 : 0}; pointer-events: {showCitation? 'auto' : 'none'}"
+    class="bg-[#9cbeaf] absolute top-0 p-2 text-sm w-[24dvw] text-left"
 >     
     {@html citation}
 </div>
 
 <a 
     class="bg-[#9cbeaf] rounded-md px-1" href={citationUrl} target="_blank"
-    on:mouseenter={(e) => { showCitation = true; positionStyle = calcPosStyle(e.clientX, e.clientY)}}
+    on:mouseenter={(e) => { calcPosStyle(e.clientX, e.clientY); showCitation = true;}}
     on:mouseleave={() => { showCitation = false }}
 >
     <slot />
